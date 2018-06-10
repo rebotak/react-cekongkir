d0eee481703b69f993d0b97b163b62347af21203
rebotak <rebotak@gmail.com>
2018-06-10 09:51:40 +0700

    set origin success

--- src/containers/CekOngkir.js
+++ src/containers/CekOngkir.js
@@ -1,7 +1,10 @@
-import React, {Component} from 'react';
-// import {load as loadPeople} from '../reducers/people';
-import {connect} from 'react-redux';
-import Loader from '../components/Loader';
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import Loader from '../components/Loader'
+import {Async} from 'react-select'
+import axios from 'axios'
+import 'react-select/dist/react-select.css' 
+
 @connect(
   (state) => ({
     // peopleData: state.people.data,
@@ -17,40 +20,68 @@ class CekOngkir extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      modal: false,
-      name: '',
-      mountedPage: 1,
-      gender: '',
-      birth_year: '',
-      eye_color: '',
-      hair_colo: ''
-    };
-    // this.toggle = this.toggle.bind(this);
+      selectedOption:''
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount(){
-    // let query = new URLSearchParams(this.props.location.search);
-    // let currentPage = query.get('page');
+    // let query = new URLSearchParams(this.props.location.search)
+    // let currentPage = query.get('page')
     // if (currentPage) {
-    //   this.setState({mountedPage: Number(currentPage)});
-    //   this.props.loadPeople(currentPage);
+    //   this.setState({mountedPage: Number(currentPage)})
+    //   this.props.loadPeople(currentPage)
     // }else{
     //   this.props.loadPeople(1)
     // }
   }
 
+  handleChange(selectedOption) {
+    this.setState({ selectedOption });
+    // selectedOption can be null when the `x` (close) button is clicked
+    if (selectedOption) {
+      console.log(selectedOption)
+      this.setState({selectedOption: selectedOption})
+    }
+  }
+
   render() {
+    // const {} = this.props
+    const { selectedOption } = this.state;
 
-    // const {} = this.props;
-    // const {} = this.state;
-
+    const getOptions = (input) => {
+      return axios.get(`https://api.zuragan.com/api/v1/pub/cities`,{
+          params:{
+            q:input,
+            min_char: 3,
+            type:'city'
+          }
+        })
+        .then((response) => {
+          let result = response.data.data.data
+          console.log(result)
+          return result;
+        })
+        .then((result) => {
+          return { options: result };
+        })
+    }
     return (
       <div className="cek-ongkir">
-        <h1>halo</h1>
-        <Loader/>
+        <h1>From: {selectedOption.name}{selectedOption && `(${selectedOption.id})`} </h1>
+        {/*<Loader/>*/}
+        <Async
+          onChange={this.handleChange}
+          labelKey="name"
+          valueKey="id"
+          name="form-field-name"
+          value={selectedOption}
+          loadOptions={getOptions}
+        />
+
       </div>
-    );
+    )
   }
 }
 
-export default CekOngkir;
+export default CekOngkir
